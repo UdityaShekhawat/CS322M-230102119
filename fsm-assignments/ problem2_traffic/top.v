0b974ce1dfbf4a990b9c1cc8fbd7cf63b801a34d
UdityaShekhawat <udityashekhawat123@gmail.com>
2025-08-18 20:06:49 +0530

    Update top.v

--- fsm-assignments/ problem2_traffic/top.v
+++ fsm-assignments/ problem2_traffic/top.v
@@ -1,12 +1,12 @@
 module top(
   input wire clk,
   input wire rst,
-output wire ; ns_g,ns_y,ns_r,
+output wire ns_g,ns_y,ns_r,
 output wire ew_g,ew_y,ew_r
-)
+);
 
 
-//Top-level(examplewiring)
+
  wire tick_1hz;
 
  tick_divider #(.CLK_FREQ_HZ(50_000_000),.TICK_HZ(1))u_div(
@@ -17,6 +17,7 @@ output wire ew_g,ew_y,ew_r
 , .ns_g(ns_g),.ns_y(ns_y),.ns_r(ns_r),.ew_g(ew_g),.ew_y(ew_y),.ew_r(ew_r)
 
  );
+
 
 
 
