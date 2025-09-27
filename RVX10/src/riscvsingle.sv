568bfab5723d35f44827fff89431d5f1fc2ca929
UdityaShekhawat <udityashekhawat123@gmail.com>
2025-09-27 16:26:13 +0530

    Update riscvsingle.sv

--- RVX10/src/riscvsingle.sv
+++ RVX10/src/riscvsingle.sv
@@ -354,7 +354,7 @@ module imem(input  logic [31:0] a,
   logic [31:0] RAM[63:0];
 
   initial
-      $readmemh("rvx10.txt",RAM);
+    $readmemh("rvx10.hex",RAM);
 
   assign rd = RAM[a[31:2]]; // word aligned
 endmodule
